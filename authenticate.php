7d8c1fc6ac8c74077d2d52a5a4d4b216c405b1ce
JhayFlourish-20 <71927501+JhayFlourish-20@users.noreply.github.com>
2022-09-01 06:09:02 +0000

    trying connection with heroku

--- authenticate.php
+++ authenticate.php
@@ -1,6 +1,21 @@
 <?php
 session_start();
 // Change this to your connection info.
+
+/*
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+*/
+
+
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
